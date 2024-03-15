188c960b107ebba84175e24adb92ee81eb9ccda3
deepesh <rddesign64@gmail.com>
2024-03-15 19:44:38 +0545

    Backend CURD

--- cart/cart.validation.js
+++
@@ -1,9 +0,0 @@
-import Yup from "yup";
-
-
-export const addItemToCartValidationSchema = Yup.object({
-  productId: Yup.string().required("Product id is required.").trim(),
-  orderedQuantity: Yup.number()
-    .required("Order quantity is required.")
-    .min(1, "Order quantity must be at least 1"),
-});
